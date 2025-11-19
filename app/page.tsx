8ce4be22edaf89e9ea0e2244f1941e14437e19df
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-11-19 13:00:43 -0500

    Debugger

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       })
 
       const data = await res.json()
-      debugger
+
       if (!res.ok) {
         const err = data?.error || 'Error al generar respuesta'
         setConversationMessages(conv.id, [
