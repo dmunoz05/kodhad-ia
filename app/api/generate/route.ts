92f2c53dd1d64b897f01653b4121d0ea6acc8a47
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-12-15 23:50:31 -0500

    Update api

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -7,48 +7,56 @@ export async function POST(req: Request) {
   try {
     const body = await req.json()
 
+    const tokenOpenRouter = process.env.NEXT_PUBLIC_OPENROUTER_API_KEY!
     const messages: Message[] = body.messages
     const model: string = body.model
-    const apiKey: string = process.env.NEXT_PUBLIC_GOOGLE_API_KEY!
+    const apiKey: string = process.env.NEXT_PUBLIC_OPENROUTER_MODEL!
 
     if (!messages) {
       return new Response(JSON.stringify({ error: "Messages required" }), { status: 400 })
     }
 
     if (!apiKey) {
-      return new Response(JSON.stringify({ error: "Missing GOOGLE_API_KEY" }), { status: 500 })
+      return new Response(JSON.stringify({ error: "Missing OPENROUTER_MODEL" }), { status: 500 })
     }
 
     const contents = messages.map(m => ({
       role: m.role,
-      parts: [{ text: m.content }],
+      content: m.content,
     }))
 
     const payload = {
-      contents,
-      generationConfig: {
-        temperature: 0.2,
-        maxOutputTokens: 512,
-      },
+      model,
+      messages: contents,
+      temperature: 0.2,
+      max_tokens: 512,
     }
 
-    const url =
-      `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    // const url =
+    //   `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+
+    const url = 'https://openrouter.ai/api/v1/chat/completions'
 
     const res = await fetch(url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Authorization": `Bearer ${tokenOpenRouter}`,
+        "Content-Type": "application/json",
+        "HTTP-Referer": "http://localhost:3000", // obligatorio
+        "X-Title": "Mi App IA Gratis"
+      },
       body: JSON.stringify(payload),
     })
 
-    if (!res.ok) {
-      const text = await res.text()
+    if (res?.error) {
+      const text = await res?.error.message
       return new Response(JSON.stringify({ error: text }), { status: 500 })
     }
 
     const data = await res.json()
 
-    const output = data.candidates?.[0].content;
+    // Adapt response based on OpenRouter/OpenAI format
+    const output = data.choices?.[0]?.message?.content;
 
     return Response.json({ content: output })
   } catch (err: unknown) {
